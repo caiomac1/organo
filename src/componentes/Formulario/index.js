9d43e4db58ab16d62882f0c7efd7f4d56d52c568
caiomac1 <caiomacedo.dev@gmail.com>
2024-01-08 13:35:34 -0300

    aulas - refatorando código duplicado, card de colaborador e mapeando os colaboradores

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -5,15 +5,6 @@ import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = (props) => {
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'DevOps',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
@@ -60,7 +51,7 @@ const Formulario = (props) => {
                 <ListaSuspensa 
                     obrigatorio = {true} 
                     label = "Time" 
-                    itens = {times} 
+                    itens = {props.times} 
                     valor = {time}
                     aoAlterado = {valor => setTime(valor)}
                 />
